5d2416f8150032f91ee802cdf3acc9786194ba0f
DJ <social.deej@gmail.com>
2021-01-26 02:06:52 +0530

    Added Student Form Model

---
+++ src/database/model/student.form.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema, Document } from 'mongoose';
+
+export const DOCUMENT_NAME = 'Student-Form';
+
+export default interface StudentForm extends Document {
+  _id: any;
+  name?: string,
+  email?: string,
+  registrationNumber?: string,
+  phoneNumber?: number,
+}
+
+const schema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: { type: String },
+  email: { type: String },
+  registrationNumber: { type: String },
+  phoneNumber: { type: Number }, 
+  // @ts-ignore
+  __v: false,
+});
+
+export const QuestionModel = model<StudentForm>(DOCUMENT_NAME, schema);
