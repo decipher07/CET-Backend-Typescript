f5e149f44339ab7acc3c6777473e3bd56cfa828a
DJ <social.deej@gmail.com>
2021-01-26 03:51:58 +0530

    Adding CheckAuth Middleware

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,6 +1,28 @@
-import {Request, Response, NextFunction} from 'express'
+import {
+  Request,
+  Response,
+  NextFunction
+} from 'express'
+import asyncHandler from '../helpers/asyncHandler'
 
-import {} from 'jsonwebtoken'
+import {verify} from 'jsonwebtoken'
+require('dotenv').config()
 
+const checkAuth = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.headers.authorization)
+    return res.status(401).json({
+      message: "Access Denied! No token entered.",
+    });
 
+  const token = req.headers.authorization.split(" ")[1];
 
+  try {
+    const verified = verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({
+      message: "Auth failed!",
+    });
+  }
+})
