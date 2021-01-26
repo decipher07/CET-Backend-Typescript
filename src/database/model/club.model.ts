a525d9c56e46cf2b6ddb28826031d26b252efbd7
DJ <social.deej@gmail.com>
2021-01-27 01:56:53 +0530

    Adding Email Checks Interface

--- src/database/model/club.model.ts
+++ src/database/model/club.model.ts
@@ -8,6 +8,7 @@ export default interface Club extends Document {
   inviteCode?: number;
   featured?: boolean;
   name: string;
+  email: string;
   username?: string;
   type?: string;
   mobileNumber?: number;
