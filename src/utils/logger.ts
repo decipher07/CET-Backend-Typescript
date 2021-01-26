96127c67633acc9415bf77d6f7312ba9f106ac0c
DJ <social.deej@gmail.com>
2021-01-27 01:29:47 +0530

    Adding Logger Specified

---
+++ src/utils/logger.ts
@@ -0,0 +1,29 @@
+const { createLogger, transports, format } = require("winston");
+const { combine, timestamp, label, printf } = format;
+
+require("dotenv").config();
+require("winston-mongodb");
+
+// @ts-ignore
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+export const errorLogger = createLogger({
+  transports: [
+    new transports.File({
+      filename: "error.log",
+      level: "error",
+      format: combine(label({ label: "ERROR" }), timestamp(), myFormat),
+    }),
+    new transports.MongoDB({
+      level: "error",
+      db: global.env.DBURI,
+      options: {
+        useUnifiedTopology: true,
+      },
+      collection: "error_logs",
+      format: format.combine(format.timestamp(), format.json()),
+    }),
+  ],
+});
