81556bbf3d5a967e6573eb276039bef94ec585b8
DJ <social.deej@gmail.com>
2021-01-28 02:07:50 +0530

    Refactoring Student Controllers

--- src/controllers/student.controllers.ts
+++ src/controllers/student.controllers.ts
@@ -3,14 +3,8 @@ import { hash, compare } from 'bcrypt'
 import { sign, verify } from 'jsonwebtoken'
 import { Types } from 'mongoose'
 import { SES } from 'aws-sdk'
-import Club, { ClubModel } from '../database/model/club.model'
-import Student, { StudentModel } from '../database/model/student.model'
-import Test, { TestModel } from '../database/model/test.model'
-import Domain, { DomainModel } from '../database/model/testDomain.model'
-import  {   sendVerificationOTP,sendWelcomeMail, shortlistedMgmt, shortlistedCC, shortlistedFrontend, shortlistedApp,
-  shortlistedBackend, shortlistedML, shortlistedCloud, shortlistedCCD3, shortlistedEditorialD3,
-  shortlistedDesignD3, shortlistedMgmtD3, // @ts-ignore
-  shortlistedEasterEgg, }  from '../utils/emailTemplates'
+import Student, { StudentModel } from '../database/model/student.model' // @ts-ignore
+import  { sendVerificationOTP,sendWelcomeMail, shortlistedMgmt, shortlistedCC, shortlistedFrontend, shortlistedApp }  from '../utils/emailTemplates'
 import {errorLogger} from '../utils/logger'
 require('dotenv').config()
 
