f1a1a0b5e4f13d5326fb358665bff72b1395b6d7
DJ <social.deej@gmail.com>
2021-01-28 02:05:49 +0530

    Refactoring Test Controllers

--- src/controllers/test.controllers.ts
+++ src/controllers/test.controllers.ts
@@ -1,18 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
-import { hash, compare } from 'bcrypt'
-import { sign, verify } from 'jsonwebtoken'
 import { Types } from 'mongoose'
-import { SES } from 'aws-sdk'
 import Club, { ClubModel } from '../database/model/club.model'
 import Question, { QuestionModel } from '../database/model/question.model'
 import Student, { StudentModel } from '../database/model/student.model'
-import StudentForm ,{StudentFormModel} from '../database/model/student.form.model'
 import Test, { TestModel } from '../database/model/test.model'
 import Domain, { DomainModel } from '../database/model/testDomain.model'
-import  {   sendVerificationOTP,sendWelcomeMail, shortlistedMgmt, shortlistedCC, shortlistedFrontend, shortlistedApp,
-  shortlistedBackend, shortlistedML, shortlistedCloud, shortlistedCCD3, shortlistedEditorialD3,
-  shortlistedDesignD3, shortlistedMgmtD3, // @ts-ignore
-  shortlistedEasterEgg, }  from '../utils/emailTemplates'
 import {errorLogger} from '../utils/logger'
 require('dotenv').config()
 
