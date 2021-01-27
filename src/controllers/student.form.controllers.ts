93371bc39572c16c33b9e4bdfb243db01171c8a9
DJ <social.deej@gmail.com>
2021-01-28 02:06:28 +0530

    Refactoring Student Form

--- src/controllers/student.form.controllers.ts
+++ src/controllers/student.form.controllers.ts
@@ -1,18 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
-import { hash, compare } from 'bcrypt'
-import { sign, verify } from 'jsonwebtoken'
 import { Types } from 'mongoose'
-import { SES } from 'aws-sdk'
-import Club, { ClubModel } from '../database/model/club.model'
-import Student, { StudentModel } from '../database/model/student.model'
 import StudentForm ,{StudentFormModel} from '../database/model/student.form.model'
-import Test, { TestModel } from '../database/model/test.model'
-import Domain, { DomainModel } from '../database/model/testDomain.model'
-import  {   sendVerificationOTP,sendWelcomeMail, shortlistedMgmt, shortlistedCC, shortlistedFrontend, shortlistedApp,
-  shortlistedBackend, shortlistedML, shortlistedCloud, shortlistedCCD3, shortlistedEditorialD3,
-  shortlistedDesignD3, shortlistedMgmtD3, // @ts-ignore
-  shortlistedEasterEgg, }  from '../utils/emailTemplates'
-import {errorLogger} from '../utils/logger'
+
 require('dotenv').config()
 
 // @desc Add Student to the student form
