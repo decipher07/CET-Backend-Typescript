1061055b5029ed0864d19fa76b3c4d793212d57b
DJ <social.deej@gmail.com>
2021-01-28 23:53:46 +0530

    Adding Auth Routes

---
+++ src/routes/auth/auth.route.ts
@@ -0,0 +1,38 @@
+import express, { Router } from 'express'
+import { authenticate } from 'passport'
+import * as cookieParser from 'cookie-parser'
+
+const router: Router = express.Router()
+
+// @ts-ignore
+router.use(cookieParser());
+
+router.get(
+  "/google",
+  authenticate("google", {
+    hd: "vitstudent.ac.in",
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
+
+///Callback route for google to redirect
+router.get(
+  "/google/redirect",
+  authenticate("google"),
+  (req, res, next) => {
+    const x = req.user;
+    console.log(req.user)// @ts-ignore
+    var token = encodeURIComponent(req.user.token);// @ts-ignore
+    var name = encodeURIComponent(req.user.name);
+    res.redirect(
+      303,
+      // "https://cet-dev.netlify.app/student/oauth/" + token + "/" + req.user.loginCount + '/'
+      // @ts-ignore
+      "http://cet-portal.codechefvit.com/student/oauth/" + token + "/" + req.user.loginCount + '/'
+      // "http://localhost:3000/student/oauth/" + token + "/"
+    );
+  }
+);
+
+export default router
