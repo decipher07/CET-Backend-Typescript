2a22eb8eebf957754567efea03d2aedb42f35b97
DJ <social.deej@gmail.com>
2021-01-29 00:41:57 +0530

    Adding Test and Domain with linking with Questions

---
+++ src/routes/test/test.route.ts
@@ -0,0 +1,61 @@
+import express, { Router } from 'express'
+
+import {create, getTestDetails, apply, attempt, submit, allApplied, allSubmitted, addStudents, publish, getAllTestOfAClub, getAllPublishedTestsOfAClub, updateTest, deleteTest } from '../../controllers/test.controllers'
+
+import checkAuth from '../../middlewares/checkAuth'
+import checkAuthStudent from '../../middlewares/checkAuthStudent'
+import checkAuthClub from '../../middlewares/checkAuthClub'
+
+
+const router: Router = express.Router()
+
+//Create a test
+router.post("/create", checkAuthClub, create);
+
+//Get details of a test by testId
+router.get("/details", checkAuth, getTestDetails);
+
+//Apply for a test
+router.post("/apply", checkAuthStudent, apply);
+
+//Attemt a test
+router.post("/attempt", checkAuthStudent, attempt);
+
+//Submit test
+router.post("/submit", checkAuthStudent, submit);
+
+//Get all applied tests
+router.get("/allApplied", checkAuthStudent, allApplied);
+
+//Get all submitted tests
+router.get("/allSubmitted", checkAuthStudent, allSubmitted);
+
+//Add users to a test
+router.post("/addStudents", checkAuthClub, addStudents);
+
+//Publish a test
+router.patch("/publish", checkAuthClub, publish);
+
+//Get all tests of a club -- admin only
+router.get(
+  "/allTestsOfAClub",
+  checkAuthClub,
+  getAllTestOfAClub
+);
+
+//Get all published tests of a club
+router.get(
+  "/allPublishedTestsOfAClub",
+  getAllPublishedTestsOfAClub
+);
+
+//Update test details
+router.patch("/details", checkAuthClub, updateTest);
+
+//Delete a test
+router.delete("/delete", checkAuthClub, deleteTest);
+
+router.use("/domain", require('../testdomain/testdomain.route'));
+
+
+export default router;
---
+++ src/routes/testdomain/testdomain.route.ts
@@ -0,0 +1,75 @@
+import express, { Router } from 'express'
+
+import { addDomain, getAllDomainsOfATest, getDetailsOfDomain, attemptDomain, submitDomain, getAllSubmissionsOfADomain, getStudentDomainSubmission, shortlistStudent, removeShortlistedStudent, publishShortlisted, updateDomainDetails, deleteDomain} from '../../controllers/testDomain.controllers'
+
+import checkAuth from '../../middlewares/checkAuth'
+import checkAuthStudent from '../../middlewares/checkAuthStudent'
+import checkAuthClub from '../../middlewares/checkAuthClub'
+
+const router: Router = express.Router()
+
+
+//Add a test domain
+router.post("/add", checkAuthClub, addDomain);
+
+//Get all domains of a test
+router.get("/all", checkAuth, getAllDomainsOfATest);
+
+//Get details of a domain
+router.get("/details", getDetailsOfDomain);
+
+//Attempt a domain
+router.post("/attempt", checkAuthStudent, attemptDomain);
+
+//Submit a domain
+router.post("/submit", checkAuthStudent, submitDomain);
+
+//Get all submissions of a domain
+router.get(
+  "/allSubmissions",
+  checkAuthClub,
+  getAllSubmissionsOfADomain
+);
+
+//Get a student's submission of a domain
+router.get(
+  "/studentSubmission",
+  checkAuthClub,
+  getStudentDomainSubmission
+);
+
+//Shortlist a student
+router.patch(
+  "/shortlist",
+  checkAuthClub,
+  shortlistStudent
+);
+
+//Remove a shortlisted student
+router.patch(
+  "/removeShortlistedStudent",
+  checkAuthClub,
+  removeShortlistedStudent
+);
+
+//Publish Shortlisted result
+router.patch(
+  "/publishShortlist",
+  checkAuthClub,
+  publishShortlisted
+);
+
+//Update domain details
+router.patch(
+  "/details",
+  checkAuthClub,
+  updateDomainDetails
+);
+
+//Delete a domain
+router.delete("/delete", checkAuthClub, deleteDomain);
+
+router.use("/question", require('../questions/questions.route'));
+
+
+export default router
